a978d6c1558adb047db76ac6c126b306a30272d0
YuJingGuo <expertweblancer.me@gmail.com>
2020-03-13 23:34:40 +0800

    Header fix

---
+++ src/Layout/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import logo from '../assets/logo.png'
+
+function Header() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light">
+            <Link className="navbar-brand" to="/">
+                <img src={logo} alt="" className="image-size"/>
+            </Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Pricing</Link>
+                    </li>                        
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Resources</Link>
+                    </li>                        
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">About</Link>
+                    </li>                        
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Contact US</Link>
+                    </li>                        
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export { Header }
