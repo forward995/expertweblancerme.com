034966070187cda722b99cb969686c22e103b623
YuJingGuo <expertweblancer.me@gmail.com>
2020-03-16 03:38:06 +0800

    header fix

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Header } from '../Layout'
-import { Link } from 'react-router-dom'
-import logo from '../assets/logo.png'
+// import { Link } from 'react-router-dom'
 
 function HomePage() {
     return(
